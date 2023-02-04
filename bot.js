97d9179e781ccac567c5346eaa78db29489cf7db
scrowe34 <seancrowe35@gmail.com>
2023-02-04 12:10:44 -0500

    asdfa

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ async function respond()
   var request = JSON.parse(this.req.chunks[0]);
   if(request.text && request.text.startsWith("Are"))
   {
-      postMessage("sync");
+      postMessage("prompting" + request.text);
      // await botCall(request.text);
   } 
 }
