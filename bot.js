7388fbde070ba00f8f58916f3a12da63140bab80
scrowe34 <seancrowe35@gmail.com>
2023-02-04 11:38:31 -0500

    asdf

--- bot.js
+++ bot.js
@@ -8,29 +8,39 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-function respond() 
+async function respond() 
 {
   var request = JSON.parse(this.req.chunks[0]);
-  if(request.text && request.text.startsWith("A"))
+  if(request.text && request.text.startsWith("Are"))
   {
-
-        this.res.writeHead(200);
-        postMessage("yes");
-        this.res.end();
-
-    
+    try {
+      const completion = await openai.createCompletion({
+        model: "text-ada-001",
+        prompt: request.text + "?",
+        temperature: 0.6,
+        max_tokens: 500
+      });
+      res.status(200).json({ result: completion.data.choices[0].text });
+      this.res.writeHead(200);
+      postMessage(completion.data.choices[0].text);
+      this.res.end();
+    } catch(error) {
+      // Consider adjusting the error handling logic for your use case
+      if (error.response) {
+        console.error(error.response.status, error.response.data);
+        res.status(error.response.status).json(error.response.data);
+      } else {
+        console.error(`Error with OpenAI API request: ${error.message}`);
+        res.status(500).json({
+          error: {
+            message: 'An error occurred during your request.',
+          }
+        });
+      }
+    } 
   } 
 }
-async function openAIResponse(message){
-  let response = await openai.createCompletion({
-    model: "text-ada-001",
-    prompt: message.slice(8)+"?",
-    temperature: 0.7,
-    max_tokens: 1000
-  });
-  console.log(response);
-  return response;
-}
+
 
 function postMessage(response) 
 {
